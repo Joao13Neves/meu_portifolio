dde9dde8e2e505a3a3a428344f29dfbeeee0d351
unknown <you@example.com>
2023-10-30 23:55:10 -0300

    feat: ajustando os inputs com js

--- principal/script.js
+++ principal/script.js
@@ -61,70 +61,84 @@ const typed = new Typed('.multiple-text', {
     loop: true
 });
 
-/*================ settings forms ================*/
-class FormSubmit {
-  constructor(settings) {
-    this.settings = settings;
-    this.form = document.querySelector(settings.form);
-    this.formbutton = document.querySelector(settings.button);
-    if (this.form) {
-      this.url = this.form.getAttribut("action");
-    }
-    this.sendForm = this.sendForm.bind(this);
-  }
 
-  displaySuccess() {
-    this.form,innerHTML = this.settings.success;
-  }
+/*================ input number mask ================*/
+const tel = document.getElementById('number') // Seletor do campo de telefone
 
-  displayError() {
-    this.form,innerHTML = this.settings.error;
-  }
+tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) // Dispara quando digitado no campo
+tel.addEventListener('change', (e) => mascaraTelefone(e.target.value)) // Dispara quando autocompletado o campo
 
-  getFormObject() {
-    const formObject = {};
-    const fields = this.form.querySelectorAll("[name]");
-    fields.forEach((field) => {
-      formObject[field.getAttribute("name")] = field.value;
-    });
-    return formObject;
-  }
-
-  onSubmission(event) {
-    event.preventDefault();
-    event.target.disabled = true;
-    event.target.innerText = "Enviando...";
-  }
-
- async sendForm (event) {
-  try {
-    this.onSubmission(event);
-    await fetch(this.url, {
-      method: "POST",
-      headers: {
-        "Content-Typed": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(this.gerFormObject()),
-    });
-    this.displaySuccess();
-  } catch (error) {
-    this.displayError();
-    throw new Error(error);
-  }
- }  
-
-  init() {
-    if (this.form) this.formbutton.addEventListener("click", this.sendForm);
-    return this;
-  } 
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "")
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
+    tel.value = valor // Insere o(s) valor(es) no campo
 }
 
-const FormSubmit = new FormSubmit({
-  form: "[data-form]",
-  button: "[data-button]",
-  success: "<h1 class='success'>Seu e-mail foi enviado com sucesso! Em breve irei entrar em contato.</h1>",
-  error: "<h1 class='error'>Não foi possível enviar sua mensagem! Tente novamente mais tarde ou entre em contato através de outra rede.</h1>",
-});;
-FormSubmit.init();
+/*================ settings forms used formsubmit.io ================*/
+// class FormSubmit {
+//   constructor(settings) {
+//     this.settings = settings;
+//     this.form = document.querySelector(settings.form);
+//     this.formbutton = document.querySelector(settings.button);
+//     if (this.form) {
+//       this.url = this.form.getAttribut("action");
+//     }
+//     this.sendForm = this.sendForm.bind(this);
+//   }
+
+//   displaySuccess() {
+//     this.form,innerHTML = this.settings.success;
+//   }
+
+//   displayError() {
+//     this.form,innerHTML = this.settings.error;
+//   }
+
+//   getFormObject() {
+//     const formObject = {};
+//     const fields = this.form.querySelectorAll("[name]");
+//     fields.forEach((field) => {
+//       formObject[field.getAttribute("name")] = field.value;
+//     });
+//     return formObject;
+//   }
+
+//   onSubmission(event) {
+//     event.preventDefault();
+//     event.target.disabled = true;
+//     event.target.innerText = "Enviando...";
+//   }
+
+//  async sendForm (event) {
+//   try {
+//     this.onSubmission(event);
+//     await fetch(this.url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Typed": "application/json",
+//         Accept: "application/json",
+//       },
+//       body: JSON.stringify(this.gerFormObject()),
+//     });
+//     this.displaySuccess();
+//   } catch (error) {
+//     this.displayError();
+//     throw new Error(error);
+//   }
+//  }  
+
+//   init() {
+//     if (this.form) this.formbutton.addEventListener("click", this.sendForm);
+//     return this;
+//   } 
+// }
+
+// const FormSubmit = new FormSubmit({
+//   form: "[data-form]",
+//   button: "[data-button]",
+//   success: "<h1 class='success'>Seu e-mail foi enviado com sucesso! Em breve irei entrar em contato.</h1>",
+//   error: "<h1 class='error'>Não foi possível enviar sua mensagem! Tente novamente mais tarde ou entre em contato através de outra rede.</h1>",
+// });;
+// FormSubmit.init();
 
